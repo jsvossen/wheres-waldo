d38c5d90fbd04200538d29c4d774a33b6a5993aa
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-06-11 14:53:53 -0400

    add target select

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
 
+	var clickX;
+	var clickY;
+
 	$('a.close').click(function(e){
 		e.preventDefault();
-		$(this).parent().fadeOut('fast');
+		$(this).closest(".popup").fadeOut('fast');
 	});
+
+	$('#board > img').click(function(e) {
+		clickX = e.pageX - $(this).offset().left;
+	    clickY = e.pageY - $(this).offset().top;
+	    showSelect();
+	});
+
+	function showSelect() {
+		var boxX = ( (clickX + $('.select').width()) > $('#board').width() ) ? ($('#board').width()-$('.select').width()) : clickX;
+		var boxY = ( (clickY + $('.select').height()) > $('#board').height() ) ? ($('#board').height()-$('.select').height()) : clickY;
+		$('.select').css({top: boxY, left: boxX }).fadeIn('fast');
+	}
 
 });
