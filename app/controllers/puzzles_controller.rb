d0eb32049fef3020487368571b30d916a9f65f1c
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-06-12 12:28:28 -0400

    track solving time

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -7,15 +7,32 @@ class PuzzlesController < ApplicationController
 	def show
 		@puzzle = Puzzle.find(params[:id])
 		@targets = @puzzle.targets
+		start_puzzle
 	end
 
 	def find_character
 		@char = Target.find(params[:cid])
 		if @char.is_at_coord?(params[:x],params[:y])
 			render json: @char.position_data
+			session[:to_find].delete(@char.id)
+			get_score if solved?
 		else
 			render json: "false"
 		end
+	end
+
+	def start_puzzle
+		session[:to_find] = @puzzle.targets.map(&:id)
+		session[:start_time] = Time.now
+	end
+
+	def solved?
+		session[:to_find].empty?
+	end
+
+	def get_score
+		session[:finish_time] = Time.now
+		puts session[:finish_time].to_time - session[:start_time].to_time
 	end
 
 end
