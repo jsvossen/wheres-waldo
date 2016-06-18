d4164b1f4661a120b679771c9f4e2493cb5ed704
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-06-18 17:31:34 -0400

    seed targets

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,16 @@
 Puzzle.where(title: "At the Beach").first_or_create do |puzzle|
 	puzzle.image = "beach-easy.jpg"
 	puzzle.difficulty = "easy"
+	puzzle.targets.build(name: "Waldo", width: 32, height: 42, x: 510, y: 348)
+	puzzle.targets.build(name: "Wizard", width: 40, height: 63, x: 608, y: 344)
+	puzzle.targets.build(name: "Odlaw", width: 22, height: 55, x: 230, y: 348)
 end
 
 
 Puzzle.where(title: "The Cake Factory").first_or_create do |puzzle|
 	puzzle.image = "cake-factory.jpg"
 	puzzle.difficulty = "medium"
+	puzzle.targets.build(name: "Waldo", width: 22, height: 30, x: 645, y: 64)
+	puzzle.targets.build(name: "Wenda", width: 25, height: 40, x: 650, y: 631)
+	puzzle.targets.build(name: "Odlaw", width: 22, height: 38, x: 331, y: 461)
 end
