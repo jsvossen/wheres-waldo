d83fb3a91b7d13dd574e36e3d45cf2789a96e6ba
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-06-19 11:19:44 -0400

    add third puzzle

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,12 @@ Puzzle.where(title: "The Cake Factory").first_or_create do |puzzle|
 	puzzle.targets.build(name: "Wenda", width: 25, height: 40, x: 650, y: 631)
 	puzzle.targets.build(name: "Odlaw", width: 22, height: 38, x: 331, y: 461)
 end
+
+Puzzle.where(title: "The Gobbling Gluttons").first_or_create do |puzzle|
+	puzzle.image = "gluttons.jpg"
+	puzzle.difficulty = "hard"
+	puzzle.targets.build(name: "Waldo", width: 20, height: 40, x: 414, y: 190)
+	puzzle.targets.build(name: "Wenda", width: 20, height: 30, x: 154, y: 169)
+	puzzle.targets.build(name: "Wizard", width: 35, height: 55, x: 810, y: 634)
+	puzzle.targets.build(name: "Odlaw", width: 20, height: 35, x: 172, y: 415)
+end
